21774f7e2af01eeb40c029f34ce9ccf95d70e56d
kk <kakas13.souza@gmail.com>
2025-12-09 19:19:01 -0300

    commit 1

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    abrirModal();
+};
+
+function abrirModal() {
+    document.getElementById("modalAuto").style.display = "flex";
+}
+
+function fecharModal() {
+    document.getElementById("modalAuto").style.display = "none";
+}
